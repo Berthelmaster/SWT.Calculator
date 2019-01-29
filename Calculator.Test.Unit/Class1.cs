6d36dffbab29fd8e811877f39e6509adbf2c8100
Berthelmaster <berthel0802@gmail.com>
2019-01-29 17:47:58 +0100

    Add project files.

---
+++ Calculator.Test.Unit/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace Calculator.Test.Unit
+{
+ [TestFixture]
+    public class CalculatorUnitTest
+    {
+        [Test]
+        public void Addition_Add2and4()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Addition(2,4), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Subtraction_Subtract6And6()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Subtraction(6,6), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Multiplication_Multiply4And4()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Multiplication(4,4), Is.EqualTo(60));
+        }
+
+        [Test]
+        public void Division_Divide60And10()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Division(60,10), Is.EqualTo(6));
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+ 
+        }
+
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static void Main()
+        {
+        }
+    }
+}
